767fb394bfe6e3df7c891daf552383cd60d346ce
Hao Peng <phshy0607@126.com>
2019-01-17 18:04:26 +0800

    add jwt

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import * as bodyParser from 'body-parser'
 import * as compression from 'compression'
 import * as express from 'express'
 import * as helmet from 'helmet'
+import * as jwt from 'jsonwebtoken'
 import * as morgan from 'morgan'
 import * as path from 'path'
 import { logsRouter } from './routers/logsRouter'
 import { engine } from './vendors/handlebars'
 import { logger, stream } from './vendors/winston'
-
+import { RequestHandler } from 'express-serve-static-core';
+/// <reference path="./index.d.ts" />
 class App {
   public instance: express.Application
   public name: String
@@ -58,12 +60,52 @@ class App {
         mode: process.env.NODE_ENV
       })
     })
-
-    this.instance.post('/githook', (req: express.Request, res: express.Response) => {
+    // ****************
+    this.instance.get('/api', (req: express.Request, res: express.Response) => {
       res.json({
-        message: 'bingo'
+        message: 'yes'
       })
     })
+    const ensureToken: RequestHandler = (req: express.Request, res: express.Response, next: express.NextFunction): void => {
+      const bearerHeader: string = req.headers.authorization
+      if (bearerHeader) {
+        const bearer: string[] = bearerHeader.split(' ')
+        const bearerToken: string = bearer[1]
+        req.token = bearerToken
+        logger.info('TOKEN =>', bearerToken)
+        next()
+      } else {
+        res.sendStatus(403)
+      }
+    }
+
+    this.instance.get('/api/protected', ensureToken, (req: express.Request, res: express.Response) => {
+      jwt.verify(req.token, 'my_secret_key', (err: jwt.VerifyErrors, data: object) => {
+        if (err) {
+          res.sendStatus(403)
+        } else {
+          res.json({
+            secret: 1,
+            data
+          })
+        }
+      })
+    })
+    this.instance.post('/api/login', (req: express.Request, res: express.Response) => {
+      const user: {id: number} = {id: 3}
+      const token: string = jwt.sign({user}, 'my_secret_key')
+      res.json({
+        token
+      })
+    })
+
+    // ********************
+
+    // this.instance.post('/githook', (req: express.Request, res: express.Response) => {
+    //   res.json({
+    //     message: 'bingo'
+    //   })
+    // })
 
     this.instance.use('/logs', logsRouter)
     logger.debug('Application routers installed......')
